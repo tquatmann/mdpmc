7ce73d36699a05cb550b5c78681459ffdb54a1fb
Tim Quatmann <tim.quatmann@cs.rwth-aachen.de>
2024-06-12 22:56:45 +0200

    show num of benchmarks and refresults for each benchmark set when postprocessing

--- scripts/postprocess.py
+++ scripts/postprocess.py
@@ -10,6 +10,8 @@ def exportData(settings, benchmark_set_id, exec_data, groups_tools_configs_sorte
     groups_tools_configs_filtered = [(g,t,c) for g,t,c in groups_tools_configs_sorted if g in exec_data and t in exec_data[g] and c in exec_data[g][t] and len(exec_data[g][t][c]) > 0]
     benchmark_set = load_json(os.path.realpath(os.path.join(sys.path[0], "data/{}.json").format(benchmark_set_id)))
     ensure_directory(benchmark_set_id)
+    benchmarks_with_ref_res = [b for b in benchmark_set if get_benchmark_from_id(settings, b).has_reference_result()]
+    print("Exporting data for benchmark set '{}' containing {} benchmarks out of which {} have a reference result".format(benchmark_set_id, len(benchmark_set), len(benchmarks_with_ref_res)))
     scatterfile = os.path.join(benchmark_set_id, settings.results_file_scatter())
     print("\tGenerating file {} for scatter plots".format(scatterfile))
     scatter_csv = generate_scatter_csv(settings, exec_data, benchmark_set, groups_tools_configs_filtered)
